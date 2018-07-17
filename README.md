e5e44dd3e204e074f9a8ae83e68b0173b8e61a38
Arnim Bleier <arnim.bleier@gmail.com>
2018-07-17 19:10:23 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Topic- and Structured Topic Modeling
+
+[![Binder](https://notebooks.gesis.org/binder/badge.svg)](https://notebooks.gesis.org/binder/v2/gh/arnim/stmdemo/master)
--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,3 @@
 install.packages("tidyverse", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 install.packages("stm", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("stmCorrViz", repos = "https://cloud.r-project.org/", dependencies=TRUE)
