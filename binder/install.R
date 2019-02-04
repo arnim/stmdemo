64550c04f6eac157f675a8961f33499e3ce783c5
Arnim Bleier <arnim.bleier@gmail.com>
2019-02-04 22:59:54 +0100

    add stan

--- binder/install.R
+++ binder/install.R
@@ -1,3 +1,4 @@
+install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 install.packages("tidyverse", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 install.packages("stm", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 install.packages("quanteda", repos='http://cran.us.r-project.org', dependencies=TRUE)
